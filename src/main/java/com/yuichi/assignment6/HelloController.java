890a99c3e67ed1e617d38b3d81d2c969f9e46cd8
u-1 <shimaichi5973@gamil.com>
2023-09-21 08:24:03 +0900

    Helloworldを実装

--- src/main/java/com/yuichi/assignment6/HelloController.java
+++ src/main/java/com/yuichi/assignment6/HelloController.java
@@ -1,4 +1,13 @@
 package com.yuichi.assignment6;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "helloworld!";
+    }
 }
